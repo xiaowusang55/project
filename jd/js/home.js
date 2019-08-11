9914d9cc7d8e52010bbe831b66acfb3c75d845a3
xiaowusang55 <Xiaowusang55>
2019-08-11 11:32:40 +0800

    modified

---
+++ jd/js/home.js
@@ -0,0 +1,12 @@
+
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+});
